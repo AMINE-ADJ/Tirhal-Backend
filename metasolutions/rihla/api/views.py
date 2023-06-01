9d71b887c17421125d87f4c2162eba2f4da61129
beraoudabdelkhalek <a.beraoud@esi-sba.dz>
2023-06-01 14:28:11 +0100

    integration2

--- metasolutions/rihla/api/views.py
+++ metasolutions/rihla/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Utilizer
-from .serializers import UtilizerSerializer,Region
+from .models import Utilizer,Place,Region,Image,Favorite
+from .serializers import UtilizerSerializer,RegionSerializer,PlaceSerializer,MiniPlaceSerializer,ImageSerializer,FavoriteSerializer
 from rest_framework import status
 
 # Create your views here.
@@ -51,3 +51,54 @@ def addregion(request):
         return Response({"msg":"success","status":status.HTTP_201_CREATED})
     except:
         return Response({"msg":"non existing admin","status":status.HTTP_400_BAD_REQUEST})
+
+    
+
+@api_view(["GET"])
+def getAllPlaceInRegion(request,id):
+    query=Place.objects.filter(idRegion=id)
+    serializer=MiniPlaceSerializer(query,many=True)
+    return Response({"data":serializer.data,"status":status.HTTP_200_OK})
+
+@api_view(["GET"])
+def getPlace(request,id):
+    try:
+        placequery=Place.objects.get(id=id)
+        if placequery:
+            imagequery=Image.objects.filter(idPlace=id)
+            imageserializer=ImageSerializer(imagequery,many=True)
+            placeserializer=PlaceSerializer(placequery)
+            return Response({'data':placeserializer.data,'image':imageserializer.data,"status":status.HTTP_200_OK})
+        else: return Response({"data":"not found","status":status.HTTP_404_NOT_FOUND})
+    except:
+        return Response({"data":"not found","status":status.HTTP_404_NOT_FOUND})
+
+@api_view(["POST"])
+def addfavourite(request):
+    idUtilizer=request.data["idUtilizer"]
+    idPlace=request.data["idPlace"]
+    utilizerquery=Utilizer.objects.get(id=idUtilizer)
+    placeerquery=Place.objects.get(id=idPlace)
+    if placeerquery and utilizerquery:
+        fav=Favorite.objects.create(idUtilizer=utilizerquery,idPlace=placeerquery)
+        if fav:
+            return Response({"msg":"added to favorites","status":status.HTTP_201_CREATED})
+        else: return Response({"msg":"something went wrong try again","status":status.HTTP_400_BAD_REQUEST})
+    else: return Response({"msg":"wrong operation","status":status.HTTP_401_UNAUTHORIZED})
+
+@api_view(["GET"])
+def getfavorite(request,id):
+    placelist=[]
+    query=Favorite.objects.filter(idUtilizer=id)
+    favoriteserilizer=FavoriteSerializer(query, many=True)
+    idplace=favoriteserilizer.data[1]["idPlace"]
+    i=0
+    for element in query:
+        idplace=favoriteserilizer.data[i]["idPlace"]
+        placelist.extend(list(Place.objects.filter(id=idplace)))
+        i=i+1
+    print(placelist)
+    serializer=PlaceSerializer(placelist,many=True)
+    return Response({"data":serializer.data})    
+
+    
